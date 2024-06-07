cc0bcdd5f1d8133487ac43a593b3d161e774ab9a
Ondřej Sluka <ondras12345@gmail.com>
2024-06-07 23:05:40 +0200

    Add project files

---
+++ README.md
@@ -0,0 +1,45 @@
+# Mikrotik-SSH-backup
+Automatically create binary backups and configuration export of Mikrotik
+RouterOS devices via SSH.
+
+
+## Setup
+Only public key SSH authentication is supported.
+Add an entry to your `~/.ssh/config` file:
+```
+Host my_router
+    User <username>
+    Hostname <ip address>
+    Port <port>
+    # optional: use a different private key file.
+    #IdentityFile ~/.ssh/id_rsa
+```
+
+You should now be able to connect to the router using this command:
+```sh
+ssh my_router
+```
+
+Create a configuration file:
+```sh
+cd ~/directory_with_mikrotik_backups
+cat << EOF > mikrotik.yaml
+routers:
+  my_router:
+EOF
+```
+(see `mikrotik-ssh-backup print_config` for all options.)
+
+Create a binary backup and an rsc export:
+```console
+$ mikrotik-ssh-backup backup --export --backup my_router
+exporting
+export written to tracked/my_router_20240607T230237.rsc
+copying to repo
+making binary backup
+Configuration backup saved
+downloading
+autobak.backup                                                                                           100%  153KB   4.3MB/s   00:00
+binary backup written to tracked/my_router_20240607T230237.backup size: 156213
+done
+```
---
+++ mikrotik_ssh_backup.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import datetime
+import os
+import sys
+import shutil
+import yaml
+from pathlib import Path
+from dataclasses import dataclass
+from pprint import pprint
+
+
+@dataclass
+class Router:
+    ssh_host: str
+    filename: str
+    # default for RouterOS 7
+    export_command: str = "/export show-sensitive"
+    backup_command: str = "/system/backup save name=autobak dont-encrypt=yes"
+
+
+def list_routers(config):
+    pprint(config["routers"])
+
+
+def print_config(config):
+    print(yaml.dump(config))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "-C", "--config-file",
+        help="path to configuration file (default: %(default)s)",
+        type=argparse.FileType("r"),
+        default="./mikrotik.yaml"
+    )
+
+    subparsers = parser.add_subparsers(dest="subparser_name", required=True)
+
+    subparsers.add_parser(
+        "list_routers",
+        help="list available routers from config file and exit"
+    )
+
+    subparsers.add_parser(
+        "print_config",
+        help="dump current config (incl. defaults) as yaml to stdout and exit"
+    )
+
+    parser_backup = subparsers.add_parser(
+        "backup",
+        help="perform backup"
+    )
+
+    parser_backup.add_argument(
+        "-d", "--diff",
+        help="Do not backup, just diff exports",
+        action="store_true"
+    )
+
+    parser_backup.add_argument(
+        "-e", "--export",
+        help="Make export (text script) backup",
+        action="store_true"
+    )
+
+    parser_backup.add_argument(
+        "-b", "--backup",
+        help="Make binary backup",
+        action="store_true"
+    )
+
+    parser_backup.add_argument(
+        "router",
+        help="router name (see list_routers)"
+    )
+
+    args = parser.parse_args()
+
+    with args.config_file as f:
+        config = yaml.safe_load(f)
+
+        config["tracked_dir"] = Path(config.get("tracked_dir", "tracked"))
+        config["repo_dir"] = Path(config.get("repo_dir", "."))
+
+        for router_name, cnf in config["routers"].items():
+            router = Router(
+                ssh_host=cnf.get("ssh_host", router_name),
+                filename=cnf.get("filename_prefix", router_name),
+            )
+            if "backup_command" in cnf:
+                router.backup_command = cnf["backup_command"]
+            if "export_command" in cnf:
+                router.export_command = cnf["export_command"]
+            config["routers"][router_name] = router
+
+    if args.subparser_name == "list_routers":
+        list_routers(config)
+        sys.exit(0)
+
+    if args.subparser_name == "print_config":
+        print_config(config)
+        sys.exit(0)
+
+    if args.router not in config["routers"]:
+        parser_backup.error(f"unknown router: {args.router}")
+
+    router = config["routers"][args.router]
+
+    if args.diff:
+        subprocess.run(
+            f'ssh {router.ssh_host} "{router.export_command}" | '
+            f'diff --color -u "{router.filename}.rsc" -',
+            shell=True
+        )
+        sys.exit(0)
+
+    time_suffix = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+    config["tracked_dir"].mkdir(parents=True, exist_ok=True)
+    filename_rsc = config["tracked_dir"] / f"{router.filename}_{time_suffix}.rsc"
+    filename_binary = config["tracked_dir"] / f"{router.filename}_{time_suffix}.backup"
+
+    if args.export:
+        print("exporting")
+        with open(filename_rsc, 'w') as f:
+            subprocess.run(
+                ["ssh", router.ssh_host, router.export_command],
+                stdout=f
+            )
+        print("export written to", filename_rsc)
+
+        print("copying to repo")
+        shutil.copyfile(filename_rsc, config["repo_dir"] / (router.filename + ".rsc"))
+
+    if args.backup:
+        print("making binary backup")
+        subprocess.run(
+            ["ssh", router.ssh_host, router.backup_command]
+        )
+        print("downloading")
+        subprocess.run(
+            ["scp", router.ssh_host+":/autobak.backup", filename_binary]
+        )
+        print("binary backup written to", filename_binary,
+              "size:", os.stat(filename_binary).st_size)
+
+    print("done")
+
+
+if __name__ == "__main__":
+    main()
